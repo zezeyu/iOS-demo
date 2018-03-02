e0074731d77d6586336d0024dfcd4715e2dba00d
heze <www.895315401@qq.com>
2018-03-02 22:15:35 +0800

    Initial Commit

---
+++ InputBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InputBox
+//
+//  Created by 何泽的mac on 2018/3/2.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
